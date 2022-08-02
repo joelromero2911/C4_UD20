261bbc8eb6dfb95520275005929ecb0d4e366a46
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-08-02 11:15:02 +0200

    ex9 v1.0

---
+++ C4_UD20_09/src/main/java/C4_UD20_09/C4_UD20_09/App.java
@@ -0,0 +1,11 @@
+package C4_UD20_09.C4_UD20_09;
+
+import graphicApp.GraphicApp;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	GraphicApp window = new GraphicApp();
+    }
+}
