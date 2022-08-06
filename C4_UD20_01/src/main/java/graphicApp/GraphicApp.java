19f17f38fe77a4f77c5d1aebc896c60f303d932c
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-08-06 19:01:46 +0200

    Ex 9 v1.0 - Correction of errors, finished what was left and documented the code

--- C4_UD20_01/src/main/java/graphicApp/GraphicApp.java
+++ C4_UD20_01/src/main/java/graphicApp/GraphicApp.java
@@ -32,13 +32,64 @@ public class GraphicApp extends JFrame{
 		JLabel label = new JLabel("Etiqueta!");
 		label.setBounds(60, 20, 187, 52);
 		label.setBorder(border);
-		label.addComponentListener(new ComponentAdapter() 
-		{ 
-		        public void componentResized(ComponentEvent evt) {
-		        	label.setBounds(getY(), getX(), getWidth(), getHeight());
-		        }
+//		label.addComponentListener(new ComponentAdapter() 
+//		{ 
+//		        public void componentResized(ComponentEvent evt) {
+//		        	label.setBounds(getY(), getX(), getWidth(), getHeight());
+//		        }
+//		});
+		label.addMouseListener(new MouseListener() {
+			
+			public void isLeftMouseButton(MouseEvent anEvent) {
+				label.setBounds(60, 20, 200, 70);
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
 		});
-		
 		contentPane.add(label);
+		
+		// Creation of a JSlider for control the JLabel.
+		JSlider slider = new JSlider();
+		slider.setSnapToTicks(true);
+		slider.setPaintTicks(true);
+		slider.setPaintLabels(true);
+		slider.setMinimum(1);
+		slider.setValue(1);
+		slider.setMaximum(5);
+		slider.setBounds(60, 342, 356, 26);
+//		slider.addChangeListener(new ChangeListener() {
+//			public void stateChanged(ChangeEvent e) {
+//				System.out.println("Slider2: " + slider2.getValue());
+//			}
+//		});
+		contentPane.add(slider);
 	}
 }
--- C4_UD20_09/src/main/java/graphicApp/GraphicApp.java
+++ C4_UD20_09/src/main/java/graphicApp/GraphicApp.java
@@ -1,6 +1,7 @@
 package graphicApp;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.event.*;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -12,8 +13,9 @@ public class GraphicApp extends JFrame{
 	
 	// Attributes.
 	private JPanel contentPane;
-	public JToggleButton tempButton =  new JToggleButton();
-	public int cardsShowed = 0;
+	private JToggleButton tempButton =  new JToggleButton();
+	private int cardsShowed = 0;
+	private int guessedPairs = 0;
 	
 	/**
 	 * Default constructor for 'GraphicApp'.
@@ -34,179 +36,198 @@ public class GraphicApp extends JFrame{
 		// Components
 		JToggleButton toggle0 = new JToggleButton();
 		toggle0.setBounds(21, 11, 99, 87);
+		toggle0.setSelected(true);
 		contentPane.add(toggle0);
 		
 		JToggleButton toggle1 = new JToggleButton();
 		toggle1.setBounds(130, 11, 99, 87);
+		toggle1.setSelected(true);
 		contentPane.add(toggle1);
 		
 		JToggleButton toggle2 = new JToggleButton();
 		toggle2.setBounds(239, 11, 99, 87);
+		toggle2.setSelected(true);
 		contentPane.add(toggle2);
 		
 		JToggleButton toggle3 = new JToggleButton();
 		toggle3.setBounds(348, 11, 99, 87);
+		toggle3.setSelected(true);
 		contentPane.add(toggle3);
 		
 		JToggleButton toggle4 = new JToggleButton();
 		toggle4.setBounds(21, 109, 99, 87);
+		toggle4.setSelected(true);
 		contentPane.add(toggle4);
 		
 		JToggleButton toggle5 = new JToggleButton();
 		toggle5.setBounds(130, 109, 99, 87);
+		toggle5.setSelected(true);
 		contentPane.add(toggle5);
 		
 		JToggleButton toggle6 = new JToggleButton();
 		toggle6.setBounds(239, 109, 99, 87);
+		toggle6.setSelected(true);
 		contentPane.add(toggle6);
 		
 		JToggleButton toggle7 = new JToggleButton();
 		toggle7.setBounds(348, 109, 99, 87);
+		toggle7.setSelected(true);
 		contentPane.add(toggle7);
 		
 		JToggleButton toggle8 = new JToggleButton();
 		toggle8.setBounds(21, 207, 99, 87);
+		toggle8.setSelected(true);
 		contentPane.add(toggle8);
 		
 		JToggleButton toggle9 = new JToggleButton();
 		toggle9.setBounds(130, 207, 99, 87);
+		toggle9.setSelected(true);
 		contentPane.add(toggle9);
 		
 		JToggleButton toggle10 = new JToggleButton();
 		toggle10.setBounds(239, 207, 99, 87);
+		toggle10.setSelected(true);
 		contentPane.add(toggle10);
 		
 		JToggleButton toggle11 = new JToggleButton();
 		toggle11.setBounds(348, 207, 99, 87);
+		toggle11.setSelected(true);
 		contentPane.add(toggle11);
 		
 		JToggleButton toggle12 = new JToggleButton();
 		toggle12.setBounds(21, 305, 99, 87);
+		toggle12.setSelected(true);
 		contentPane.add(toggle12);
 		
 		JToggleButton toggle13 = new JToggleButton();
 		toggle13.setBounds(130, 305, 99, 87);
+		toggle13.setSelected(true);
 		contentPane.add(toggle13);
 		
 		JToggleButton toggle14 = new JToggleButton();
 		toggle14.setBounds(239, 305, 99, 87);
+		toggle14.setSelected(true);
 		contentPane.add(toggle14);
 		
 		JToggleButton toggle15 = new JToggleButton();
 		toggle15.setBounds(348, 305, 99, 87);
+		toggle15.setSelected(true);
 		contentPane.add(toggle15);
 		
+		// Arrays that will be used along the code.
 		JToggleButton[] toggleButtons = {toggle0, toggle1, toggle2, toggle3, toggle4, toggle5, toggle6, toggle7, toggle8, toggle9, toggle10, toggle11, toggle12, toggle13, toggle14, toggle15};
 		Color[] colorsAssigned = new Color[16];
 		Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.CYAN, Color.YELLOW, Color.ORANGE, Color.PINK, Color.DARK_GRAY};
-		System.out.print("0");
 
-		asignColorsRandomly(colorsAssigned, colors);
+		asignColorsRandomly(toggleButtons, colorsAssigned, colors); // Asigns colors from a list of colors, until there is 2 buttons of each color.
 		
+		// Adding the action of calling 'showColor(JToggleButton btn)' method to all the JToggleButtons.
 		toggle0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle0 ,colorsAssigned[0]);
+				showColor(toggle0);
 			}
 		});
 		
 		toggle1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle1 ,colorsAssigned[1]);
+				showColor(toggle1);
 			}
 		});
 		
 		toggle2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle2 ,colorsAssigned[2]);
+				showColor(toggle2);
 			}
 		});
 		
 		toggle3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle3 ,colorsAssigned[3]);
+				showColor(toggle3);
 			}
 		});
 
 		toggle4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle4 ,colorsAssigned[4]);
+				showColor(toggle4);
 			}
 		});
 		
 		toggle5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle5 ,colorsAssigned[5]);
+				showColor(toggle5);
 			}
 		});
 		
 		toggle6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle6 ,colorsAssigned[6]);
+				showColor(toggle6);
 			}
 		});
 		
 		toggle7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle7 ,colorsAssigned[7]);
+				showColor(toggle7);
 			}
 		});
 		
 		toggle8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle8 ,colorsAssigned[8]);
+				showColor(toggle8);
 			}
 		});
 		
 		toggle9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle9 ,colorsAssigned[9]);
+				showColor(toggle9);
 			}
 		});
 		
 		toggle10.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle10 ,colorsAssigned[10]);
+				showColor(toggle10);
 			}
 		});
 
 		toggle11.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle11 ,colorsAssigned[11]);
+				showColor(toggle11);
 			}
 		});
 		
 		toggle12.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle12 ,colorsAssigned[12]);
+				showColor(toggle12);
 			}
 		});
 		
 		toggle13.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle13 ,colorsAssigned[13]);
+				showColor(toggle13);
 			}
 		});
 		
 		toggle14.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle14,colorsAssigned[14]);
+				showColor(toggle14);
 			}
 		});
 		
 		toggle15.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				showColor(toggle15 ,colorsAssigned[15]);
+				showColor(toggle15);
 			}
 		});
 	
 	}
 	
 	/**
-	 * PUT METHOD EXPLENATION <---------------------------------------
+	 * Asigns colors from a list of colors, until there is 2 buttons of each color.
+	 * 
+	 * @param toggleButtons 
 	 * @param colorsAsigned array of Colors that will have the colors assigned to the buttons 
 	 * @param colors array with all the colors available
 	 */
-	public void asignColorsRandomly(Color[] colorsAssigned, Color[] colors) {
+	public void asignColorsRandomly(JToggleButton[] toggleButtons, Color[] colorsAssigned, Color[] colors) {
 		
 		int randomNum;
 		
@@ -218,51 +239,73 @@ public class GraphicApp extends JFrame{
 					randomNum = (int) Math.floor(Math.random() * colorsAssigned.length);
 				} while (colorsAssigned[randomNum] != null);
 				
-				colorsAssigned[randomNum] = colors[i]; 
+				colorsAssigned[randomNum] = colors[i];
+				toggleButtons[randomNum].setBackground(colors[i]);
 			}
 		}
 
 	}
 	
 	/**
-	 *Method that shows a button color  |[Quitar]  @joel <------- Funciona MAL (Los colores no desapracen a veces y A veces se hacen match cuando no tienen que hacerse)
-	 * @param btn The button that will show the color
-	 * @param colors Color of the btn
+	 * Method that shows the color asigned to the button, compares buttons when 2 clicked, to take the necessary action.
+	 * 
+	 * @param btn -> The button that will show the color.
+	 * @param colors -> Color of the btn.
 	 */	
-	public void showColor (JToggleButton btn ,Color color) {
+	public void showColor (JToggleButton btn) {
 		
-		btn.setSelected(false); //Deselect the button to show the real color
-		btn.setBackground(color);
-		
-		if(tempButton.getBackground() == btn.getBackground()) { //Check if the background of the last button is the same as this
-				
-				JOptionPane.showMessageDialog(null, "Pareja Adivinada!!");
-				//Set the carts showed to 0 and disables the buttons
-				cardsShowed = 0;
-				btn.setEnabled(false);
-				tempButton.setEnabled(false);
-				tempButton = new JToggleButton(); // reset of the tempbutton
-		}
-	
-		cardsShowed ++;
-		if(cardsShowed == 2 ) {//If are 2 cards shown in this point that would mean that the user haven't guess the two buttons with the same color so it hides them
-			cardsShowed = 0;
+		if(!tempButton.toString().equals(btn.toString())) {
 			
-			hideColor(btn);
-		}else {
-			tempButton = btn;
+			btn.setEnabled(false);
+			
+			// Checks if the background of the previous button is the same of the actual one, preventing the user from clicing the same button 2 times.
+			if(tempButton.getBackground() == btn.getBackground() && !tempButton.toString().equals(btn.toString())) {
+					
+					JOptionPane.showMessageDialog(null, "Pareja Adivinada!!");
+					guessedPairs++;
+					
+					// If the used got all the pairs, a pop up will show telling that the game is finished.
+					if(guessedPairs == 8) {
+						JOptionPane.showMessageDialog(null, "Has acabado el juego!!");
+					}
+
+					btn.setVisible(false); // Makes disappear the guessed pair.
+					tempButton.setVisible(false);
+			}
+			cardsShowed ++;
+			
+			// When 2 buttons have been selected the following block of code its triggered.
+			if(cardsShowed == 2) {
+				
+				// Disable all buttons to prevent the user from bugging the GraphicApp.
+				for (Component c : contentPane.getComponents()) {
+					if(c instanceof JToggleButton) {
+						c.setEnabled(false);
+					}
+				}
+				
+				// Start a timer to give the user some time to watch the color of the selected buttons.
+				Timer timer = new Timer(800, new ActionListener(){
+					public void actionPerformed( ActionEvent e ){
+						cardsShowed = 0;
+						btn.setSelected(true); // Hides the color of the button again.
+						tempButton.setSelected(true);
+						tempButton = new JToggleButton(); // Reset of the tempButton.
+						
+						// Reenable all buttons to continue the game.
+						for (Component c : contentPane.getComponents()) {
+							if(c instanceof JToggleButton) {
+								c.setEnabled(true);
+							}
+						}
+					}
+				});
+				timer.setRepeats(false);
+				timer.start();
+				
+			}else {
+				tempButton = btn; // Saves the button when its the 1st selected of the pair to compare it with the next one clicked.
+			}
 		}
 	}
-	
-	/**
-	 * Method that hide a button color
-	 * @param btn The button that will hide it's color
-	 */
-	public void hideColor (JToggleButton btn) /*throws InterruptedException*/ {
-//        Thread.sleep(1000);
-		btn.setSelected(true);
-		tempButton.setSelected(true);
-
-	}
-	
 }
